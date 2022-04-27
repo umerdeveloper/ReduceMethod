9ee112bf75a212b128fea3b9b43f1a1a8d8096b5
Umer Khan <39640717+umerdeveloper@users.noreply.github.com>
2022-04-27 04:20:27 +0300

    Update reduce.swift

--- reduce.swift
+++ reduce.swift
@@ -1,1 +1,27 @@
+// Reduce function
+// Returns the result of combining the elements of the sequence using the given closure
 
+// Problem
+// I want to calculate total items including replies
+
+struct Comment {
+  let message: String
+  let replies: [String]
+}
+
+var comments = [Comment]()
+var totalComments: Int { comments.count }
+var totalItems = 0
+
+// Before
+var repliesCounter = 0
+comments.forEach { comment in 
+    let totalReplies = comment.replies.count
+    repliesCounter += totalReplies
+}
+totalItems = totalComments + repliesCounter
+
+// After
+let totalRepliesForEachComment = comments.map { $0.replies.count }
+let totalReplies = totalRepliesForEachComment.reduce(0, +)
+totalItems = totalComments + totalReplies
